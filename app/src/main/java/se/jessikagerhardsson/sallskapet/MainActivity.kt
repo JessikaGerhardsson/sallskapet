e8eec24d04a4fcbed28ce92860042d9d9f4a95d9
Jessika Gerhardsson <jessika_hej@hotmail.com>
2022-03-14 13:13:22 +0100

    image views

--- app/src/main/java/se/jessikagerhardsson/sallskapet/MainActivity.kt
+++ app/src/main/java/se/jessikagerhardsson/sallskapet/MainActivity.kt
@@ -2,13 +2,29 @@ package se.jessikagerhardsson.sallskapet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Simon says
+        val game = findViewById<ImageView>(R.id.gameImageView)
+
+        //memory
+        val pair = findViewById<ImageView>(R.id.pairImageView)
+
+        //bokstav mot ord
+        val match = findViewById<ImageView>(R.id.matchImageView)
+
+        //räkna spindlarna
+        val spider = findViewById<ImageView>(R.id.spiderImageView)
+
+        //hur många hundar
+        val dog = findViewById<ImageView>(R.id.dogsImageView)
+
     }
 }
 
-// Testar github
-//Nytt test
