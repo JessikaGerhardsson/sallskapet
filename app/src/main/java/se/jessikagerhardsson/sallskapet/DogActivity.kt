676c4356f2e444b8cdf1132d7f28cc8c4dee0ef0
Jessika Gerhardsson <jessika_hej@hotmail.com>
2022-03-15 09:31:50 +0100

    räkna hundar - koden

--- app/src/main/java/se/jessikagerhardsson/sallskapet/DogActivity.kt
+++ app/src/main/java/se/jessikagerhardsson/sallskapet/DogActivity.kt
@@ -2,10 +2,131 @@ package se.jessikagerhardsson.sallskapet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+
+class Animal {
+    var imagenumber = 0
+    var animalname = ""
+}
 
 class DogActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dog)
+
+
+        var animals = mutableListOf<Animal>()
+        var correctPosition = 0
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_dog)
+
+            //val images = mutableListOf<Int>(R.drawable.hund1, R.drawable.gris, R.drawable.star)
+            //val ord = mutableListOf<String>("hund", "Gris", "Stjärna")
+
+            var animal1 = Animal()
+            animal1.imagenumber = R.drawable.dog1
+            animal1.animalname = "1"
+            animals.add(animal1)
+            var animal2 = Animal()
+            animal2.imagenumber = R.drawable.dog2
+            animal2.animalname = "2"
+            animals.add(animal2)
+            var animal3 = Animal()
+            animal3.imagenumber = R.drawable.dog3
+            animal3.animalname = "3"
+            animals.add(animal3)
+
+            //var correctanswer = mutableListOf<Int>()
+            //correctanswer.add((0..2).random())
+
+            var bild1 = findViewById<ImageView>(R.id.bild)
+            var ord1 = findViewById<TextView>(R.id.ord1)
+            var ord2 = findViewById<TextView>(R.id.ord2)
+            var ord3 = findViewById<TextView>(R.id.ord3)
+
+            newword()
+
+            ord1.setOnClickListener {
+                if(correctPosition == 0)
+                {
+                    // HURRA RÄTT
+                    correctword()
+                } else {
+                    // BU FEL
+                    wrongword()
+                }
+            }
+            ord2.setOnClickListener {
+                if(correctPosition == 1)
+                {
+                    // HURRA RÄTT
+                    correctword()
+                } else {
+                    // BU FEL
+                    wrongword()
+                }
+            }
+            ord3.setOnClickListener {
+                if(correctPosition == 2)
+                {
+                    // HURRA RÄTT
+                    correctword()
+                } else {
+                    // BU FEL
+                    wrongword()
+                }
+            }
+        }
+
+        fun correctword()
+        {
+            Log.i("PIXDEBUG", "RÄTT")
+            newword()
+        }
+
+        fun wrongword()
+        {
+            Log.i("PIXDEBUG", "FEL")
+
+        }
+
+        fun newword()
+        {
+            var bild1 = findViewById<ImageView>(R.id.bild)
+            var ord1 = findViewById<TextView>(R.id.ord1)
+            var ord2 = findViewById<TextView>(R.id.ord2)
+            var ord3 = findViewById<TextView>(R.id.ord3)
+
+
+            animals.shuffle()
+
+            correctPosition = (0..2).random()
+            if(correctPosition == 0)
+            {
+                ord1.text = animals[0].animalname
+                ord2.text = animals[1].animalname
+                ord3.text = animals[2].animalname
+            }
+            if(correctPosition == 1)
+            {
+                ord1.text = animals[1].animalname
+                ord2.text = animals[0].animalname
+                ord3.text = animals[2].animalname
+            }
+            if(correctPosition == 2)
+            {
+                ord1.text = animals[2].animalname
+                ord2.text = animals[1].animalname
+                ord3.text = animals[0].animalname
+            }
+
+            bild1.setImageResource(animals[0].imagenumber)
+        }
+
+        fun clickWord()
+        {
+
+        }
+
     }
-}
--- app/src/main/java/se/jessikagerhardsson/sallskapet/MainActivity.kt
+++ app/src/main/java/se/jessikagerhardsson/sallskapet/MainActivity.kt
@@ -3,6 +3,7 @@ package se.jessikagerhardsson.sallskapet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 
 //TODO Bilderna på startskärmen syns inte
@@ -18,27 +19,34 @@ class MainActivity : AppCompatActivity() {
         game.setOnClickListener {
             val intent = Intent(this, SimonsaysActivity::class.java)
             startActivity(intent)
+            Log.i("HEJ", "simon says")
         }
 
         //Hittaparen
         val pair = findViewById<ImageView>(R.id.pairImageView)
         pair.setOnClickListener {
             val intent = Intent(this, HittaparenActivity::class.java)
+            intent.putExtra("knapp", 1)
             startActivity(intent)
+            Log.i("HEJ", "hitta paren")
         }
 
         //bokstav mot ord
         val match = findViewById<ImageView>(R.id.matchImageView)
         match.setOnClickListener {
-            val intent = Intent(this, BokstavmotdjurActivity::class.java)
+            val intent = Intent(this, HittaparenActivity::class.java)
+            intent.putExtra("knapp", 2)
             startActivity(intent)
+            Log.i("HEJ", "bokstav mot djur")
         }
 
         //räkna spindlarna
         val spider = findViewById<ImageView>(R.id.spiderImageView)
         spider.setOnClickListener {
-            val intent = Intent(this, SpiderActivity::class.java)
+            val intent = Intent(this, HittaparenActivity::class.java)
+            intent.putExtra("knapp", 3)
             startActivity(intent)
+            Log.i("HEJ", "spindlar")
         }
 
         //hur många hundar
@@ -46,6 +54,7 @@ class MainActivity : AppCompatActivity() {
         dog.setOnClickListener {
             val intent = Intent(this, DogActivity::class.java)
             startActivity(intent)
+            Log.i("HEJ", "hundar")
         }
 
     }
